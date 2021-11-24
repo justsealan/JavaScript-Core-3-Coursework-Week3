3bfe097cc71983431e187a5a4efd70d5f12c4eef
justsealan <justsealan@gmail.com>
2021-11-24 20:47:43 +0000

    Complete exercise 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,17 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+function printReceipt(order) {
+  let total = 0;
+  console.log("QTY     ITEM                TOTAL");
+  for (let i = 0; i < order.length; i++) {
+    const item = order[i];
+    const { itemName, quantity, unitPrice } = item;
+    const totalPrice = quantity * unitPrice;
+    total += totalPrice;
+    console.log(`${quantity}       ${itemName}           ${totalPrice.toFixed(2)}`);
+  }
+  console.log(`Total: ${total.toFixed(2)}`);
+}
+printReceipt(order);
